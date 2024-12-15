bdde23d416cbb6d5858502eac29d34fc896798bc
artorias305 <caique.andraus@gmail.com>
2024-12-15 20:12:43 -0300

    kanagawa theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
       vim.g.sonokai_transparent_background = "1"
       vim.g.sonokai_enable_italic = "1"
       vim.g.sonokai_style = "andromeda"
-      vim.cmd.colorscheme("sonokai")
+      -- vim.cmd.colorscheme("sonokai")
     end,
   },
   {
@@ -16,5 +16,17 @@ return {
   {
     "rose-pine/neovim",
     name = "rose-pine",
+  },
+  {
+    "sainnhe/gruvbox-material",
+  },
+  {
+    "aktersnurra/no-clown-fiesta.nvim",
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      vim.cmd.colorscheme("kanagawa-dragon")
+    end,
   },
 }
